1fa5044475980083cef29bb652d1074bacae7d5e
gnavien <gnavien@gmail.com>
2024-10-07 11:50:57 +0530

    update

--- userdata.sh
+++ userdata.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
 
+
+set-hostname -skip-apply ${component}
 labauto ansible
-ansible-pull -i localhost, -U https://github.com/gnavien/roboshop-ansible main.yml role_name=rabbitmq -e env=${env} &>>/opt/ansible.log
+ansible-pull -i localhost, -U https://github.com/gnavien/roboshop-ansible main.yml -e role_name=rabbitmq -e env=${env} &>>/opt/ansible.log
