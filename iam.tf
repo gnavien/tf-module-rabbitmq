008037d2b47048b8d1d58cc63f316fa1ba0f679c
gnavien <gnavien@gmail.com>
2024-12-02 18:52:28 +0530

    Updated new aws account info

--- iam.tf
+++ iam.tf
@@ -20,7 +20,7 @@ resource "aws_iam_policy" "policy" {
                 "ssm:GetParameter",
                 "kms:Decrypt"
              ]
-         "Resource": [ "arn:aws:ssm:us-east-1:968585591903:parameter/roboshop-${var.env}.${var.component}.*", var.kms_key_arn
+         "Resource": [ "arn:aws:ssm:us-east-1:438465144813:parameter/roboshop-${var.env}.${var.component}.*", var.kms_key_arn
         ]
       }
     ]
